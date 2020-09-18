de9d80aa8d1a40a1876e4bc2fa59bf465a58017d
chefeleUNH <chefele@newhaven.edu>
2020-09-17 20:01:39 -0400

    added first logging statement

--- app/src/main/java/edu/newhaven/icebreaker_class/MainActivity.kt
+++ app/src/main/java/edu/newhaven/icebreaker_class/MainActivity.kt
@@ -2,10 +2,16 @@ package edu.newhaven.icebreaker_class
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "onCreate fired")
     }
 }
