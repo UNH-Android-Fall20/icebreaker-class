f4d392246ad3fba8c1d8ac17ab375c9c87b938f0
chefeleUNH <chefele@newhaven.edu>
2020-09-24 22:06:43 -0400

    adding firebase and first collection read

--- app/src/main/java/edu/newhaven/icebreaker_class/MainActivity.kt
+++ app/src/main/java/edu/newhaven/icebreaker_class/MainActivity.kt
@@ -3,14 +3,28 @@ package edu.newhaven.icebreaker_class
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = javaClass.name
 
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db.collection("questions")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
 
         Log.i(TAG, "Pushing data to Firebase")
     }
